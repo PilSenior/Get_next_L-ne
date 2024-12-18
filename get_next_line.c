ada2eab4582174a6265a51fd04345c51d7dcf3ea
PilSenior <clear@TEKDEMIR.>
2024-12-18 07:23:22 +0300

    2102

---
+++ get_next_line.c
@@ -0,0 +1,108 @@
+#include "get_next_line.h"
+#include <unistd.h>
+static char     *slice(char *s, char c)
+{
+    char    *str;
+    int i;
+    int j;
+    
+    i = 0;
+    j = 0;
+
+    while(s[i] && s[i] != c)
+        i++;
+    if(!s[i])
+    {
+        free(s);
+        return (NULL);
+    }
+    str = (char *)malloc((ft_strlen(s) - i) + 1);
+    if(!str)
+        return(NULL);
+    i++;
+    while(s[i])
+        str[j++] = s[i++];
+    str[j] = '\0';
+    free(s);
+    return(str);
+}
+
+static char *new_line(char *s, char c)
+{
+    char    *str;
+    int i;
+
+    i = 0;
+    if(!s[i])
+        return(NULL);
+    while(s[i] != '\0' && s[i] != c)
+        i++;
+    str = (char *)malloc(sizeof(char) * (i + 2));
+    if(!str)
+        return(NULL);
+    i = 0;
+    while(s[i] && s[i] != c)
+    {
+        str[i] = s[i];
+        i++;
+    }
+    if(s[i] == c)
+    {
+        str[i] = s[i];
+        i++;
+    }
+    str[i] = '\0';
+    return(str);
+}
+
+static char     *table(int fd, char *s)
+{
+    char *str;
+    int i;
+    str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+    if(!str)
+    {
+        if(s != (void *)0)
+            free(s);
+        return(NULL);
+    }   
+    i = 1;
+    while(!ft_strchr(s, '\n') && i != 0)
+    {
+        i = read(fd, str, BUFFER_SIZE);
+        if(i == -1)
+        {
+            free(str);
+            if(s)
+                free(s);
+            return(NULL);
+           
+        }
+        str[i] = '\0';
+        s = ft_strjoin(s, str);
+        if(s == 0)
+        {
+            return(NULL);
+        }
+    }
+    free(str);
+    return(s);
+}
+
+char    *get_next_line(int fd)
+{
+    char    *str;
+    static char *line;
+
+    if(fd < 0 || BUFFER_SIZE <= 0)
+        return(NULL);
+    line = table(fd, line);
+    if(line)
+    {
+        str = new_line(line, '\n');
+        line = slice(line, '\n');
+        return(str);
+    }
+    
+    return(NULL);
+}
